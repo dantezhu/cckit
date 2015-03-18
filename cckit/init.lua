3ee90e65f561baeaa17c7d843b59716b7c12d59f
dantezhu <dantezhu@qq.com>
2015-03-18 17:41:43 +0800

    visiblesize又正常了

--- cckit/init.lua
+++ cckit/init.lua
@@ -1,21 +1,7 @@
 cckit = cckit or {}
 
 function cckit.fillScreen(node)
-    local resolutionPolicy = cc.Director:getInstance():getOpenGLView():getResolutionPolicy()
-    local resolutionSize = cc.Director:getInstance():getOpenGLView():getDesignResolutionSize()
-
-    assert(resolutionPolicy == cc.ResolutionPolicy.FIXED_WIDTH or resolutionPolicy == cc.ResolutionPolicy.FIXED_HEIGHT)
-    -- 真实设备分辨率只能这样获取
-    -- getWinSize 之类在调用完 setDesignResolutionSize 之后就完全不对了
-    local frameSize = cc.Director:getInstance():getOpenGLView():getFrameSize()
-    
-    if resolutionPolicy == cc.ResolutionPolicy.FIXED_WIDTH then
-        -- 务必计算比例，因为是 FIX_WIDTH，所以以设计WIDTH为准算出HEIGHT
-        node:setContentSize(cc.size(resolutionSize.width, frameSize.height * resolutionSize.width / frameSize.width))
-    else
-        node:setContentSize(cc.size(frameSize.width * resolutionSize.height / frameSize.height, resolutionSize.height))
-    end
-
+    node:setContentSize(cc.Director:getInstance():getVisibleSize())
     ccui.Helper:doLayout(node)
 end
 
